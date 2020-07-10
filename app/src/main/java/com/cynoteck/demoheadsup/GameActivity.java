131f09047c108c0ec8c23b4e1da79458e1aac0cd
ashish rawat <ashishrawat6677@gmail.com>
2020-07-10 13:51:50 +0530

    HeadsAct

--- app/src/main/java/com/cynoteck/demoheadsup/GameActivity.java
+++ app/src/main/java/com/cynoteck/demoheadsup/GameActivity.java
@@ -58,7 +58,6 @@ public class GameActivity extends FragmentActivity implements SensorEventListene
         soundInit();
         gameAndPauseTime();
         init();
-        gameAndPauseTime();
         firstCountDownStart();
 
     }
